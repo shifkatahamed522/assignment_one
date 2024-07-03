a6bbde3450a8979e4d43a879c74d34b02f0623a1
shifkatahamed522 <shifkatahamed522@gmail.com>
2024-07-03 16:44:12 +0600

    finish

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,9 @@
+<?php
+// Temperature in Celsius
+$celsius = 32;
+
+// Conversion formula: Fahrenheit = (Celsius * 9/5) + 32
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// Print result up to 2 decimal points
+printf("The temperature is %.2f degrees Fahrenheit.", $fahrenheit);
